10db65e78cb2be3678f14d8814d2082383bde568
Yunchi Pang <yunchipang@gmail.com>
2023-07-13 15:55:32 -0700

    Construct Home, Library and Profile views (#2)

---
+++ MoveMakers/Views/LibraryView.swift
@@ -0,0 +1,14 @@
+//
+//  LibraryView.swift
+//  MoveMakers
+//
+//  Created by Yunchi Pang on 7/13/23.
+//
+
+import SwiftUI
+
+struct LibraryView: View {
+    var body: some View {
+        Text("Library view")
+    }
+}
